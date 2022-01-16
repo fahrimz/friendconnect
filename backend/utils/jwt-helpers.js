e70b33b8b1da5dd641d8d7cba1a6c84f160a0e56
fahrimz <mzfahri620@gmail.com>
2022-01-16 10:37:58 +0700

    chore: change jwt expire time to 30 days

--- backend/utils/jwt-helpers.js
+++ backend/utils/jwt-helpers.js
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken';
 //Generate an access token and a refresh token for this database user
 function jwtTokens({ id_user, username, invite_code, avatar_url }) {
   const user = { id_user, username, invite_code, avatar_url }; 
-  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1000s' });
+  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '30d' });
   return ({ accessToken });
 }
 
